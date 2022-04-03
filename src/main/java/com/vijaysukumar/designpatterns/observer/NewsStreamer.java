2b9137bd5f4f5386cca2b50cdaaddf70e23ca868
Vijay Sukumar <vijayanand.sukumar@gmail.com>
2022-04-03 13:33:56 -0700

    thread safe

--- src/main/java/com/vijaysukumar/designpatterns/observer/NewsStreamer.java
+++ src/main/java/com/vijaysukumar/designpatterns/observer/NewsStreamer.java
@@ -3,6 +3,7 @@ package com.vijaysukumar.designpatterns.observer;
 import java.util.*;
 
 public class NewsStreamer {
+
     public static void main(String[] args) {
         ArrayList<String> strings = new ArrayList<String>();
         strings.add("Enter data to be processed. Ctrl-C to stop ");
@@ -24,10 +25,15 @@ public class NewsStreamer {
 
 class News{
     String data;
-    List<Observer> observers = new ArrayList<>();
+    Set<Observer> observers = new HashSet<>();
+
+    //object used for lock
+    Object MONITOR = new Object();
 
     void addObserver(Observer observer){
-        observers.add(observer);
+        synchronized (MONITOR) {
+            observers.add(observer);
+        }
     }
 
     void setData(String data){
@@ -36,7 +42,12 @@ class News{
     }
 
     void notifyAllObservers(){
-        for(Observer observer : observers){
+
+        Set<Observer>  copyObservers = new HashSet<>();
+        synchronized (MONITOR) {
+            copyObservers = new HashSet<>(observers);
+        }
+        for (Observer observer : copyObservers) {
             observer.update();
         }
     }
